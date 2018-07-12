a73d68f9c3e966008957ca5e5498fdb4ebc64715
Jorge Arenas <jorshx@gmail.com>
2018-07-11 23:29:40 -0500

    Initial Commit

---
+++ list-of-urls/main.js
@@ -0,0 +1,5 @@
+let arr = [
+    { name: "Safari Books", link: "https://www.safaribooksonline.com"},
+    { name: "Egghead", link: "https://egghead.io"},
+    { name: "Lynda", link: "https://www.lynda.com"},
+];
