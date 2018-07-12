a0331ace502bb80499e974682d05f45e449e48dd
Jorge Arenas <jorshx@gmail.com>
2018-07-11 23:32:49 -0500

    Added solution for list-of-urls

--- list-of-urls/main.js
+++ list-of-urls/main.js
@@ -3,3 +3,20 @@ let arr = [
     { name: "Egghead", link: "https://egghead.io"},
     { name: "Lynda", link: "https://www.lynda.com"},
 ];
+
+let list = document.createElement('ul');
+list.id = 'myLinks';
+console.log(list);
+document.body.appendChild(list);
+
+arr.forEach( item => {
+    let link = document.createElement('a');
+    link.textContent = item.name;
+    link.href = item.link;
+
+    let listItem = document.createElement('li');
+    listItem.appendChild(link);
+
+    list.appendChild(listItem);
+});
+
